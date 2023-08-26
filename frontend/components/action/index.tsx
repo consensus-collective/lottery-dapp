f7e15e52bc5ec1917ddb2bed4a42293eea87a209
Abdul Hakim <hakim@undercurrent.tech>
2023-08-26 13:54:28 +0700

    added message component

--- frontend/components/action/index.tsx
+++ frontend/components/action/index.tsx
@@ -1,4 +1,6 @@
-import BuyToken from "./buy";
+import { useState } from "react";
+import { BuyToken } from "./buy";
+import { Message, MessageProps as IMessage } from "./message";
 
 import dynamic from "next/dynamic";
 import styles from "./action.module.css";
@@ -6,11 +8,30 @@ import styles from "./action.module.css";
 const PlaceBets = dynamic(() => import("./bets"), { ssr: false });
 
 export default function Action() {
+  const [data, setData] = useState<IMessage>({
+    message: "",
+    status: "",
+  });
+
+  const onChangeMessage = (message: string, status: string, url?: string) => {
+    setData({
+      ...data,
+      message,
+      status,
+      url,
+    });
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.bets}>
-        <BuyToken />
-        <PlaceBets />
+      <div className={styles.bets_container}>
+        <div className={styles.bets}>
+          <BuyToken onChangeMessage={onChangeMessage} />
+          <PlaceBets />
+        </div>
+        <div className={styles.message_container}>
+          <Message {...data} />
+        </div>
       </div>
     </div>
   );
