3abf7cdaef610aeb9ed48eac1f2acc86fe7a085a
Abdul Hakim <hakim@undercurrent.tech>
2023-08-26 22:24:00 +0700

    refactor buy and bet functionality

--- frontend/components/action/index.tsx
+++ frontend/components/action/index.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { BuyToken } from "./buy";
-import { Message, MessageProps as IMessage } from "./message";
 
 import dynamic from "next/dynamic";
 import styles from "./action.module.css";
@@ -8,30 +6,11 @@ import styles from "./action.module.css";
 const PlaceBets = dynamic(() => import("./bets"), { ssr: false });
 
 export default function Action() {
-  const [data, setData] = useState<IMessage>({
-    message: "",
-    status: "",
-  });
-
-  const onChangeMessage = (message: string, status: string, url?: string) => {
-    setData({
-      ...data,
-      message,
-      status,
-      url,
-    });
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.bets_container}>
-        <div className={styles.bets}>
-          <BuyToken onChangeMessage={onChangeMessage} />
-          <PlaceBets onChangeMessage={onChangeMessage} />
-        </div>
-        <div className={styles.message_container}>
-          <Message {...data} />
-        </div>
+        <BuyToken />
+        <PlaceBets />
       </div>
     </div>
   );
